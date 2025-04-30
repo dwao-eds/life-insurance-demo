b77d2741d8ba780e31455a9c1767e0cdd1629c93
dwao-eds <rahulaggarwal@dwao.co.in>
2025-05-01 00:11:47 +0530

    fix body

--- blocks/blog-body/blog-body.js
+++ blocks/blog-body/blog-body.js
@@ -13,13 +13,19 @@ export default function decorate(block) {
       row.classList.add('blog-data');
     }
     if (r === 4) {
-      row.classList.add('blog-foot');
-    }
+      row.classList.add('blog-faq');
+        const paragraphs = row.querySelectorAll("p");
 
-    // [...row.children].forEach((div, d) => {
-    //   if (d === 0) {
-    //     div.classList.add('class-name');
-    //   }
-    // });
+        paragraphs.forEach((p) => {
+          if (p.querySelector("strong")) {
+            p.classList.add("faq-ques");
+          } else {
+            p.classList.add("faq-ans");
+          }
+        });
+    }
+    if (r === 5) {
+      row.classList.add("blog-foot");
+    }
   });
 }
