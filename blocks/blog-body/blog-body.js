32ed5f576bb7a86ae7137326eaf185ee944d2e3d
dwao-eds <rahulaggarwal@dwao.co.in>
2025-04-30 14:46:44 +0530

    FIXES

--- blocks/blog-body/blog-body.js
+++ blocks/blog-body/blog-body.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function decorate(block) {
   [...block.children].forEach((row, r) => {
     if (r === 0){
@@ -23,30 +22,4 @@ export default function decorate(block) {
     //   }
     // });
   });
-=======
-export default function decorate(block) {
-  [...block.children].forEach((row, r) => {
-    if (r === 0){
-    row.classList.add('blog-pill');
-    }
-    if (r === 1) {
-      row.classList.add('blog-heading');
-    }
-    if (r === 2) {
-      row.classList.add('blog-date');
-    }
-    if (r === 3) {
-      row.classList.add('blog-data');
-    }
-    if (r === 4) {
-      row.classList.add('blog-foot');
-    }
-
-    // [...row.children].forEach((div, d) => {
-    //   if (d === 0) {
-    //     div.classList.add('class-name');
-    //   }
-    // });
-  });
->>>>>>> 703d5d047a70cb59571ef50d2a69be8ba2813288
 }
